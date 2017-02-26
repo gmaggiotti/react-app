35f690a99bc939160fe7915bda97407406bfde20
gabe <gmaggiotti@gmail.com>
2017-02-26 08:33:10 -0300

    first commit

---
+++ src/GabeApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//Class component can have state
+class Gabe extends React.Component {
+    render() {
+        return <h1>Hello statefull</h1>
+    }
+}
+
+//Stateless function component
+//const Gabe  = () => <h1>Hello stateless</h1>
+
+export default Gabe;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GabeApp from './GabeApp';
+
+
+ReactDOM.render(
+  <GabeApp />,
+  document.getElementById('root')
+);
