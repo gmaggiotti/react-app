883d45c85d5a17da50abb174a57bdcbad9a60c74
gmaggiotti <gmaggiotti@gmail.com>
2017-04-09 14:47:46 -0300

    avoid mutations using  slice() and concat()

--- src/redux-9.js
+++ src/redux-9.js
@@ -22,11 +22,9 @@ const removeCounter = (list, index) => {
 };
 
 const incrementCounter = (list, index) => {
-    return [
-        ...list.slice(0, index),
-        list[index] + 1,
-        ...list.slice(index + 1)
-    ];
+    return [...list.slice(0,index),
+            ++list[index],
+        ...list.slice(index+1)];
 };
 
 const testAddCounter = () => {
